d1c920c35a4c73b669aaf03a63cecf3dc23a4963
Serge <Serge@DESKTOP-2DU4BCS.home>
2019-04-07 17:32:39 +0200

    chm

--- src/Model/AStarThread.java
+++ src/Model/AStarThread.java
@@ -9,8 +9,6 @@ public class AStarThread implements Runnable{
 	private volatile boolean running = false;
 	private Thread t;
 
-//quatrieme ligneeeeeeeeeeeeeeeeeeeeeeeee luderic est un gros kik
-	// mm
 
 	public AStarThread(Game g, Player p, int x, int y) {
 		this.g = g;

cf21b0cf6be5030b7938af8162adb22ab313b180
BuildTools <unconfigured@null.spigotmc.org>
2019-04-07 17:41:06 +0200

    changements de Game et Keyboard

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -6,9 +6,9 @@ import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Random;
 
-import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
+//import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
 
-import org.omg.CosNaming.IstringHelper;
+//import org.omg.CosNaming.IstringHelper;
 
 public class Game implements DeletableObserver {
     private ArrayList<GameObject> objects = new ArrayList<GameObject>();

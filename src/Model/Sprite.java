0d28b615b4dc9296ae3dc5c750eec9d883e99fa2
Serge <Serge@DESKTOP-2DU4BCS.home>
2019-04-11 15:48:46 +0200

    p

---
+++ src/Model/Sprite.java
@@ -0,0 +1,26 @@
+package Model;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Sprite {
+	private BufferedImage img = null;
+	private String path;
+	
+	public Sprite(String path) {
+		this.path = path;
+		try {
+	    	   img = ImageIO.read(new File(this.path));
+	       }catch(IOException e) {
+	    	   e.printStackTrace();
+	    	   }
+	}
+	
+	public BufferedImage getImage() {
+		return this.img;
+	}
+	
+}
